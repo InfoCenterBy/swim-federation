7c6274b377bcd5ad6d78cc00ba831a220725d62d
dyamorph <dyamorph22@gmail.com>
2024-09-06 08:30:35 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const searchBtn = document.querySelector('.search-btn');
+  const searchInput = document.querySelector('.search-input');
+  const links = document.querySelectorAll('.header .link') 
+
+  searchBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    links.forEach(link => {
+      link.classList.toggle('hidden')
+    })
+    
+    searchInput.classList.toggle('active')
+
+    if (searchInput.classList.contains('active')) {
+      searchInput.focus();
+    } else {
+      searchInput.blur(); 
+    }
+  });
+});
