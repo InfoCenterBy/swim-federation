455169e4e28c0feee52f180545bda5667f33020f
dyamorph <dyamorph22@gmail.com>
2024-10-01 10:41:26 +0300

    Updates

--- js/cabinet-script.js
+++ js/cabinet-script.js
@@ -530,3 +530,82 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const cabinetMobileMenu = document.querySelector(".cabinet__mobile-menu");
+  const cabinetMobileProfile = document.querySelector(".cabinet__mobile-profile");
+  const profileButton = document.querySelector(".cabinet__profile-btn");
+  const body = document.body;
+
+  if (menuToggle) {
+    menuToggle.addEventListener("click", function () {
+      menuToggle.classList.toggle("open");
+      cabinetMobileMenu.classList.toggle("open");
+
+      if (cabinetMobileProfile.classList.contains("open")) {
+        cabinetMobileProfile.classList.remove("open");
+      }
+
+      if (cabinetMobileMenu.classList.contains("open")) {
+        body.classList.add("no-scroll");
+      } else {
+        body.classList.remove("no-scroll");
+      }
+    });
+  }
+
+  if (cabinetMobileProfile) {
+    profileButton.addEventListener("click", function () {
+      cabinetMobileProfile.classList.toggle("open");
+
+      if (cabinetMobileMenu.classList.contains("open")) {
+        cabinetMobileMenu.classList.remove("open");
+        menuToggle.classList.remove("open");
+      }
+
+      if (cabinetMobileProfile.classList.contains("open")) {
+        body.classList.add("no-scroll");
+      } else {
+        body.classList.remove("no-scroll");
+      }
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("modal-window-exit");
+  const closeButton = document.getElementById("modal-close-btn");
+  const cancelButton = document.querySelector(".cancel-btn");
+  const exitBtns = document.querySelectorAll(".exit-btn");
+  const body = document.body;
+
+  if (modal) {
+    exitBtns.forEach((btn) =>
+      btn.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.add("open");
+      })
+    );
+
+    closeButton.addEventListener("click", function () {
+      modal.classList.remove("open");
+    });
+
+    cancelButton.addEventListener("click", function () {
+      modal.classList.remove("open");
+    });
+
+    if (modal.classList.contains("open")) {
+      body.classList.add("no-scroll");
+    } else {
+      body.classList.remove("no-scroll");
+    }
+
+    window.addEventListener("click", function (event) {
+      if (event.target === modal) {
+        modal.classList.remove("open");
+      }
+    });
+  }
+});
+
