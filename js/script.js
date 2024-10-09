de9a0a657edfff5f56590a1182ac75190be98620
dyamorph <dyamorph22@gmail.com>
2024-10-09 16:30:06 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -328,3 +328,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+const slider = new Swiper(".slider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    330: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 70,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
