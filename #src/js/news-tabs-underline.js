9f0ccbc7ddf5f4a67e677cdc3192926ffca054e4
dyamorph <dyamorph22@gmail.com>
2024-10-10 18:02:06 +0300

    add news page

--- #src/js/news-tabs-underline.js
+++ #src/js/news-tabs-underline.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".news__tabs");
-  const underline = document.querySelector(".nav-underline");
+  function tabsUnderline() {
+    const container = document.querySelector(".news__tabs");
+    const underline = document.querySelector(".nav-underline");
 
-  if (container) {
-  function updateUnderlineWidth() {
-    const scrollWidth = container.scrollWidth;
-    const clientWidth = container.clientWidth;
-    const scrollX = container.scrollLeft;
+    if (container) {
+      function updateUnderlineWidth() {
+        const scrollWidth = container.scrollWidth;
+        const clientWidth = container.clientWidth;
+        const scrollX = container.scrollLeft;
 
-    underline.style.setProperty("--scroll-x", scrollX);
-    underline.style.setProperty("--scroll-max", scrollWidth - clientWidth);
+        underline.style.setProperty("--scroll-x", scrollX);
+        underline.style.setProperty("--scroll-max", scrollWidth - clientWidth);
+      }
+
+      container.addEventListener("scroll", updateUnderlineWidth);
+      updateUnderlineWidth();
+    }
   }
-
-    container.addEventListener("scroll", updateUnderlineWidth);
-    updateUnderlineWidth();
-  }
+  tabsUnderline();
 });
