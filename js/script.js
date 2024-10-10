b2341774b1b4ca98fd94352e28482ecf9e25354d
dyamorph <dyamorph22@gmail.com>
2024-10-10 08:19:38 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -330,21 +330,17 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const slider = new Swiper(".slider", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  // Responsive breakpoints
   breakpoints: {
-    // when window width is >= 320px
     330: {
       slidesPerView: 1,
       spaceBetween: 20,
     },
-    // when window width is >= 480px
+
     480: {
       slidesPerView: 1,
       spaceBetween: 30,
     },
-    // when window width is >= 640px
+
     640: {
       slidesPerView: 1,
       spaceBetween: 70,
