3dfd0deace2eea8032cfc1d7a40f5a72c16ed24d
dyamorph <dyamorph22@gmail.com>
2024-10-15 11:27:38 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -354,3 +354,30 @@ const slider = new Swiper(".slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const selects = document.querySelectorAll(".form-select");
+
+if (selects) {
+  selects.forEach((select) => {
+    select.addEventListener("change", function () {
+      if (select.value === "") {
+        select.style.color = "#797979";
+      } else {
+        select.style.color = "#1d1d1d";
+      }
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hash = window.location.hash;
+
+  if (hash) {
+    const tabTriggerEl = document.querySelector('button[data-bs-target="' + hash + '"]');
+
+    if (tabTriggerEl) {
+      let tab = new bootstrap.Tab(tabTriggerEl);
+      tab.show();
+    }
+  }
+});
