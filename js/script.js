dc1e7bd4838f7fcdacdba616c9e9c0b6ba8a0dcf
dyamorph <dyamorph22@gmail.com>
2024-10-24 08:42:42 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -355,12 +355,23 @@ const slider = new Swiper(".slider", {
   },
 });
 
-if (window.innerWidth <= 768) {
+const isMobile = window.innerWidth <= 768;
+
+const isFirefox = navigator.userAgent.toLowerCase().includes("firefox");
+
+if (isMobile && isFirefox) {
   document.querySelectorAll(".dropdown").forEach(function (dropdown) {
+    const menu = dropdown.querySelector(".dropdown-menu");
+
     dropdown.addEventListener("click", function (e) {
       e.preventDefault();
-      const menu = this.querySelector(".dropdown-menu");
       menu.classList.toggle("show");
+    });
+
+    document.addEventListener("click", function (event) {
+      if (!dropdown.contains(event.target)) {
+        menu.classList.remove("show");
+      }
     });
   });
 }
