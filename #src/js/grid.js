7d5eb11d41fe524e58b848b0ed5e1ce0d9ab260b
dyamorph <dyamorph22@gmail.com>
2024-10-28 17:53:22 +0300

    grid update

--- #src/js/grid.js
+++ #src/js/grid.js
@@ -1,8 +1,4 @@
-// Grid API: Access to Grid API methods
-
-// Grid Options: Contains all of the grid configurations
 const gridOptions = {
-  // Data to be displayed
   rowData: [
     { make: "Tesla", model: "Model Y", price: 64950, electric: true },
     { make: "Ford", model: "F-Series", price: 33850, electric: false },
@@ -11,43 +7,33 @@ const gridOptions = {
     { make: "Fiat", model: "500", price: 15774, electric: false },
     { make: "Nissan", model: "Juke", price: 20675, electric: false },
   ],
-  // Columns to be displayed (Should match rowData properties)
+
   columnDefs: [{ field: "make" }, { field: "model" }, { field: "price" }, { field: "electric" }],
   defaultColDef: {
     flex: 1,
   },
-  // onColumnMoved: () => saveColumnState,
-  onColumnMoved: () => console.log("123"),
-  onColumnResized: () => saveColumnState,
-  onSortChanged: () => saveColumnState,
-  onGridReady: () => loadColumnState,
+
+  onColumnMoved: (params) => saveColumnState(params),
+  onColumnResized: () => console.log("456"),
+  onSortChanged: () => console.log("789"),
+  onGridReady: (params) => {
+    const columnApi = params.api;
+
+    const columnState = columnApi.getColumnState();
+
+    console.log("Текущее состояние колонок:", columnState);
+
+    localStorage.setItem("columnState", JSON.stringify(columnState));
+  },
 };
 
-console.log(gridOptions);
+function saveColumnState(params) {
+  console.log("123");
 
-function saveColumnState() {
-  const columnState = gridOptions.columnApi.getColumnState();
-  localStorage.setItem("columnState", JSON.stringify(columnState));
-}
-
-function loadColumnState() {
-  const savedState = localStorage.getItem("columnState");
-  if (savedState) {
-    gridOptions.columnApi.applyColumnState({
-      state: JSON.parse(savedState),
-      applyOrder: true,
-    });
-  }
-}
-
-// Загружаем состояние при первой инициализации
-loadColumnState();
-
-function clearColumnState() {
-  localStorage.removeItem("columnState");
-  gridOptions.columnApi.resetColumnState();
+  const columnState = params.api.getColumnState();
+  localStorage.setItem("agColumnState", JSON.stringify(columnState));
 }
 
 const ediv = document.querySelector("#myGrid");
-// Create Grid: Create new grid within the #myGrid div, using the Grid Options object
+
 const gridApi = agGrid.createGrid(ediv, gridOptions);
